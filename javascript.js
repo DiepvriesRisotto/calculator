6cd12eeda55030dba6a1632244b01f5052814620
DiepvriesRisotto <95715044+DiepvriesRisotto@users.noreply.github.com>
2023-09-28 11:08:21 +0200

    Setup and add operator functions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+function operate(number1, operator, number2) { 
+
+}
